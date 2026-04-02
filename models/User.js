106f18ff30ca54bd5e9a7fd808879d59de38459a
rahulkewat001 <rahulkewat68734@gmail.com>
2026-04-02 11:53:36 +0530

    Fix: Resolve User model pre-save hook and enhance CORS configuration

--- models/User.js
+++ models/User.js
@@ -25,15 +25,10 @@ const userSchema = new mongoose.Schema({
   },
 }, { timestamps: true });
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  try {
-    const salt = await bcrypt.genSalt(12);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 userSchema.methods.matchPassword = function (plain) {
